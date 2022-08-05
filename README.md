20a10d20f67c21c5471be7b37d4d4a398cd7d031
Mohamed Elsayed <m.elsaied.930@gmail.com>
2022-08-05 10:10:28 -0500

    commit

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Terraform
 
-% brew install terraform
-% terraform -v
-% terraform init
-% terraform plan
-% terraform apply
-% terraform apply -help   
-% terraform apply -auto-approve 
-% terraform destroy
-% terraform state list
-% terraform show# Terraform
+brew install terraform
+terraform -v
+terraform init
+terraform plan
+terraform apply
+terraform apply -help
+terraform apply -auto-approve
+terraform destroy
+terraform destroy -auto-approve
+terraform state list
+terraform show# Terraform
+terraform fmt
--- main.tf
+++ main.tf
@@ -1,8 +1,70 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     "name" = "dev"
+  }
+}
+
+resource "aws_subnet" "main_public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+  tags = {
+    "name" = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    "name" = "dev-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "main_default_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    "name" = "dev-default-route-table"
+  }
+}
+
+resource "aws_route" "main_default_route" {
+  route_table_id         = aws_route_table.main_default_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+}
+
+resource "aws_route_table_association" "main_route_table_association" {
+  subnet_id      = aws_subnet.main_public_subnet.id
+  route_table_id = aws_route_table.main_default_route_table.id
+}
+
+resource "aws_security_group" "main_security_group" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    "name" = "dev-security-group"
   }
 }
