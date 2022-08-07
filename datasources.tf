7d1eda4f00fa4ead1f34b5414427c0f8776d6c13
Mohamed Elsayed <m.elsaied.930@gmail.com>
2022-08-06 22:24:44 -0500

    Create EC2 and Install Docker engin

---
+++ datasources.tf
@@ -0,0 +1,8 @@
+data "aws_ami" "dev_ami" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+}
--- main.tf
+++ main.tf
@@ -1,70 +1,80 @@
-resource "aws_vpc" "main_vpc" {
+resource "aws_vpc" "dev_vpc" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    "name" = "dev"
+    "name" = "dev_vpc"
   }
 }
 
-resource "aws_subnet" "main_public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
+resource "aws_subnet" "dev_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
   cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2a"
   tags = {
-    "name" = "dev-public"
+    "name" = "dev_subnet"
   }
 }
 
-resource "aws_internet_gateway" "main_internet_gateway" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_internet_gateway" "dev_internet-gateway" {
+  vpc_id = aws_vpc.dev_vpc.id
   tags = {
-    "name" = "dev-internet-gateway"
+    "name" = "dev_internet-gateway"
   }
 }
 
-resource "aws_route_table" "main_default_route_table" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_route_table" "dev_route-table" {
+  vpc_id = aws_vpc.dev_vpc.id
   tags = {
-    "name" = "dev-default-route-table"
+    "name" = "dev_route-table"
   }
 }
 
-resource "aws_route" "main_default_route" {
-  route_table_id         = aws_route_table.main_default_route_table.id
+resource "aws_route" "dev_route" {
+  route_table_id         = aws_route_table.dev_route-table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+  gateway_id             = aws_internet_gateway.dev_internet-gateway.id
 }
 
-resource "aws_route_table_association" "main_route_table_association" {
-  subnet_id      = aws_subnet.main_public_subnet.id
-  route_table_id = aws_route_table.main_default_route_table.id
+resource "aws_route_table_association" "dev_route-table-association" {
+  subnet_id      = aws_subnet.dev_subnet.id
+  route_table_id = aws_route_table.dev_route-table.id
 }
 
-resource "aws_security_group" "main_security_group" {
+resource "aws_security_group" "dev_security-group" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main_vpc.id
-
+  vpc_id      = aws_vpc.dev_vpc.id
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
+resource "aws_key_pair" "dev_key-pair" {
+  key_name   = "key-pair-2"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "dev_instance" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.dev_ami.id
+  key_name               = aws_key_pair.dev_key-pair.id
+  vpc_security_group_ids = [aws_security_group.dev_security-group.id]
+  subnet_id              = aws_subnet.dev_subnet.id
+  user_data              = file("userdata.tpl")
   tags = {
-    "name" = "dev-security-group"
+    "name" = "dev_instance"
   }
 }
